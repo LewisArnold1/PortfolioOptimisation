0cadbcd98587c1f39b7e0247b881d8aca79fba13
LewisArnold1 <185296117+LewisArnold1@users.noreply.github.com>
2024-12-23 21:06:48 +0000

    Created displayStockData function

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,18 @@
 
 using json = nlohmann::json;
 
+void displayStockData(const json& stockData) {
+    for (auto& [symbol, price] : stockData.items()) {
+        std::cout << symbol << ": " << price << std::endl;
+    }
+}
+
 int main() {
     std::cout << "Welcome to Portfolio Optimiser!" << std::endl;
 
-    json stockData = {{"AAPL", 177.30}};
-    std::cout << stockData.dump() << std::endl;
+    // JSON object for stock data
+    json stockData = {{"AAPL", 177.30}, {"MSFT", 315.40}, {"GOOG", 2845.50}};
+    displayStockData(stockData);
     
     return 0;
 }
